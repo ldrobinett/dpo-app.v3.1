f53eeb011cf407e590339277cc853a32913746e5
ldrobinett <lonnierobinett@msn.com>
2026-07-25 21:16:02 -0700

    Export MI database entities and shared foundation

--- mi_v5/database/__init__.py
+++ mi_v5/database/__init__.py
@@ -1,1 +1,29 @@
 """Shared persistence foundation for Management Intelligence v5."""
+
+from .entities import MIEntity, TenantEntity
+from .mixins import (
+    AuditMixin,
+    EffectiveDateMixin,
+    SoftRetirementMixin,
+    StatusMixin,
+    TenantMixin,
+    UUIDPrimaryKeyMixin,
+    VersionMixin,
+)
+from .naming import NAMING_CONVENTION
+from .types import UUID_TYPE, new_uuid
+
+__all__ = [
+    "AuditMixin",
+    "EffectiveDateMixin",
+    "MIEntity",
+    "NAMING_CONVENTION",
+    "SoftRetirementMixin",
+    "StatusMixin",
+    "TenantEntity",
+    "TenantMixin",
+    "UUIDPrimaryKeyMixin",
+    "UUID_TYPE",
+    "VersionMixin",
+    "new_uuid",
+]
