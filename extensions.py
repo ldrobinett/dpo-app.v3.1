35f8fd2b289b7fbf1656601b5a7fd0ae56e1838e
ldrobinett <lonnierobinett@msn.com>
2026-07-25 21:04:02 -0700

    Apply stable SQLAlchemy metadata naming convention

--- extensions.py
+++ extensions.py
@@ -2,8 +2,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
+from sqlalchemy import MetaData
 
-db = SQLAlchemy()
+from mi_v5.database.naming import NAMING_CONVENTION
+
+
+db = SQLAlchemy(metadata=MetaData(naming_convention=NAMING_CONVENTION))
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'  # IMPORTANT: blueprint-aware
 bcrypt = Bcrypt()
@@ -13,6 +17,7 @@ migrate = Migrate()
 # Flask-Login user loader (CRITICAL)
 # -------------------------------------------------
 from models import User, OperatorUser
+
 
 @login_manager.user_loader
 def load_user(user_id):
