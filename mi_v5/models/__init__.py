528057df852c568a58a20621c27a63c8589fa757
ldrobinett <lonnierobinett@msn.com>
2026-08-01 10:58:41 -0700

    Export Organizational Group models

--- mi_v5/models/__init__.py
+++ mi_v5/models/__init__.py
@@ -1,5 +1,13 @@
 """Management Intelligence v5 persisted business entities."""
 
 from .enterprise import Enterprise
+from .organizational_group import (
+    OrganizationalGroup,
+    OrganizationalGroupHierarchy,
+)
 
-__all__ = ["Enterprise"]
+__all__ = [
+    "Enterprise",
+    "OrganizationalGroup",
+    "OrganizationalGroupHierarchy",
+]
