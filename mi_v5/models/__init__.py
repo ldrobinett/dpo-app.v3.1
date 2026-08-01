2566de26c009dd654d1e07ddcaf9d344b83c0bcc
ldrobinett <lonnierobinett@msn.com>
2026-08-01 11:21:35 -0700

    Export Managed Store aggregate models

--- mi_v5/models/__init__.py
+++ mi_v5/models/__init__.py
@@ -1,6 +1,7 @@
 """Management Intelligence v5 persisted business entities."""
 
 from .enterprise import Enterprise
+from .managed_store import ManagedStore, ManagedStoreGroupMembership
 from .organizational_group import (
     OrganizationalGroup,
     OrganizationalGroupHierarchy,
@@ -8,6 +9,8 @@ from .organizational_group import (
 
 __all__ = [
     "Enterprise",
+    "ManagedStore",
+    "ManagedStoreGroupMembership",
     "OrganizationalGroup",
     "OrganizationalGroupHierarchy",
 ]
