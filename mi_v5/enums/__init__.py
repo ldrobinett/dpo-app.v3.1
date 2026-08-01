6cf8fcb4094d126687d8570a2829d3abccb048a3
ldrobinett <lonnierobinett@msn.com>
2026-08-01 11:37:04 -0700

    Export Department enums

--- mi_v5/enums/__init__.py
+++ mi_v5/enums/__init__.py
@@ -1,11 +1,15 @@
 """Business-language enumerations for Management Intelligence v5."""
 
+from .department_status import DepartmentStatus
+from .department_type import DepartmentType
 from .enterprise_status import EnterpriseStatus
 from .managed_store_status import ManagedStoreStatus
 from .organizational_group_status import OrganizationalGroupStatus
 from .organizational_group_type import OrganizationalGroupType
 
 __all__ = [
+    "DepartmentStatus",
+    "DepartmentType",
     "EnterpriseStatus",
     "ManagedStoreStatus",
     "OrganizationalGroupStatus",
