cbf24a2336a42fb914c97d81433bf0ea4047f0ac
ldrobinett <lonnierobinett@msn.com>
2026-08-01 11:21:25 -0700

    Export Managed Store status enum

--- mi_v5/enums/__init__.py
+++ mi_v5/enums/__init__.py
@@ -1,11 +1,13 @@
 """Business-language enumerations for Management Intelligence v5."""
 
 from .enterprise_status import EnterpriseStatus
+from .managed_store_status import ManagedStoreStatus
 from .organizational_group_status import OrganizationalGroupStatus
 from .organizational_group_type import OrganizationalGroupType
 
 __all__ = [
     "EnterpriseStatus",
+    "ManagedStoreStatus",
     "OrganizationalGroupStatus",
     "OrganizationalGroupType",
 ]
