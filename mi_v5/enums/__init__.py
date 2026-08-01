198d70720ca5bef2127bae12fccc549460e60b6d
ldrobinett <lonnierobinett@msn.com>
2026-08-01 10:58:10 -0700

    Export organizational group enums

--- mi_v5/enums/__init__.py
+++ mi_v5/enums/__init__.py
@@ -1,5 +1,11 @@
 """Business-language enumerations for Management Intelligence v5."""
 
 from .enterprise_status import EnterpriseStatus
+from .organizational_group_status import OrganizationalGroupStatus
+from .organizational_group_type import OrganizationalGroupType
 
-__all__ = ["EnterpriseStatus"]
+__all__ = [
+    "EnterpriseStatus",
+    "OrganizationalGroupStatus",
+    "OrganizationalGroupType",
+]
